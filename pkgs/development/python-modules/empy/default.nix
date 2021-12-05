70a161a4bffa7e72c0b02e39ff72019f9ff39a4b
nkalupahana <avishkank@gmail.com>
2021-12-04 22:21:30 -0600

    Add self to maintainers, and add package to top-level

--- pkgs/development/python-modules/empy/default.nix
+++ pkgs/development/python-modules/empy/default.nix
@@ -7,10 +7,10 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "c6xJeFtgFHnfTqGKfHm8EwSop8NMArlHLPEgauiPAbM=";
   };
-  meta = {
+  meta = with lib; {
     homepage = "http://www.alcyone.com/software/empy/";
     description = "A templating system for Python.";
-    maintainers = "Erik Max Francis";
-    license = lib.licenses.lgpl21Only;
+    maintainers = with maintainers; [ nkalupahana ];
+    license = licenses.lgpl21Only;
   };
 }
